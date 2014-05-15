bda9edcff1f509e544204c52738322d405876f2d
wzaghal <wisamzaghal@gmail.com>
2014-05-15 10:22:07 -0400

    Testing for heroku deployment

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var router = express.Router(); //get an instance of the express Router
 
 router.use(function(req, res, next){
 	//do logging
-	console.log('Something is happening');
+	console.log('Something is happening. Test:' + process.argv[2]);
 	next(); //make sure we go to the next routes and don't stop here
 });
 
