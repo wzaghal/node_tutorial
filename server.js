fb0fdff72b5dce1fb0fdbb328f88e7ada12aebca
wzaghal <wisamzaghal@gmail.com>
2014-05-15 10:25:14 -0400

    Testing heroku for deployment

--- server.js
+++ server.js
@@ -27,14 +27,14 @@ var router = express.Router(); //get an instance of the express Router
 
 router.use(function(req, res, next){
 	//do logging
-	console.log('Something is happening. Test:' + process.argv[2]);
+	console.log('Something is happening');
 	next(); //make sure we go to the next routes and don't stop here
 });
 
 
 //test router to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res){
-	res.json({ message: 'hooray! welcome to our api'});
+	res.json({ message: 'hooray! welcome to our api. Test:' + process.argv[2]});
 
 });
 
